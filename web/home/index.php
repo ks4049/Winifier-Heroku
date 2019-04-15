f024083e1b6187ad0c1e8ed254d0961c2c751c4c
nirbhay <pherwani37@gmail.com>
2019-04-14 23:07:13 -0400

    menu text changes

--- web/home/index.php
+++ web/home/index.php
@@ -49,7 +49,7 @@
                                     <h4 class="header-title m-t-0 m-b-30">Information Retrieval</h4>
 
                                     <p class="text-muted m-b-15 font-15">
-                                        <center>Retrieve reviews based on keywords and or filters. Filter further with visualized word cloud!</center>
+                                        <center>Retrieve reviews based on keywords and or filters. <b>Filter further with visualized word cloud! <span class="badge badge-info">In Progress!</span><b></center>
                                     </p>
                                     <button type="button" class="btn btn-block btn-xs btn-purple waves-effect waves-light" onclick="location.href='/home/info/index.php';">Search Now</button>
 
